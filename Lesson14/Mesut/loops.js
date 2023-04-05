86f9183c43fe41f865162cbaa7277f8ac174c161
Mesut OZTURK <doktormesut@gmail.com>
2023-04-05 22:59:11 +0300

    answers

--- Lesson14/Mesut/loops.js
+++ Lesson14/Mesut/loops.js
@@ -1,16 +1,39 @@
 // 1. Write a loop that prints out the numbers from 1 to 10.
 
+for(let i=1; i<11; i++){
+    console.log(i)
+}
+
 console.log('- - - - - - - - - - - - - - - - - ');
 // 2. Write a loop that prints out the odd numbers from 1 to 20.
 // 1,3,5,7...
+for(let i=1; i<21; i+=2){
+    console.log(i)
+}
 
 console.log('- - - - - - - - - - - - - - - - - ');
 // 3. Write a loop that prints out the even numbers from 1 to 20.
 // 2, 4, 6...
 
+for (let i= 2; i<21; i +=2 ){
+    console.log(i)
+}
+
 console.log('- - - - - - - - - - - - - - - - - ');
 // 4. Write a loop that finds the maximum number in an array of numbers.
-const findMaxNumberInArray = [3, 10, 23, 42, 55, 12, 7, 4, 90];
+
+// const findMaxNumberInArray = [3, 10, 23, 42, 55, 12, 7, 4, 90];
+// let maxNumberInArray= findMaxNumberInArray[0]
+
+// for(let i=0; i<findMaxNumberInArray.length; i++);
+
+// if (findMaxNumberInArray[i]> maxNumberInArray) {
+//     console.log();
+    
+// }
+
+
+
 
 console.log('- - - - - - - - - - - - - - - - - ');
 // 5. Write a loop that finds the minimum number in an array of numbers.
@@ -22,6 +45,10 @@ const string = 'Lorem ipsum dolor sit amet consectetur adipisicing elit.';
 
 console.log('- - - - - - - - - - - - - - - - - ');
 // 7. Write a while loop that counts from 10 to 1 and prints out each number.
+
+for(let i=10; i>0; i--){
+    console.log(i);
+}
 
 console.log('- - - - - - - - - - - - - - - - - ');
 // 8. Write a while loop that generates random numbers between 1 and 10 until a number greater than 8 is generated, and then prints out the total number of iterations.
