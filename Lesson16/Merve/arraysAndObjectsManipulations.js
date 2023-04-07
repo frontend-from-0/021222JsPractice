912fe72b42a9c726399942d809965574a8c8208a
Anna S. A <anna.atalay@minna.tech>
2023-04-07 21:53:09 +0200

    Add Lesson16 directory

---
+++ Lesson16/Merve/arraysAndObjectsManipulations.js
@@ -0,0 +1,75 @@
+// We did not manage to do the exercises in the Lesson 15 so we do them in the lesson 16 instead. This is the reason Lesson15 directory is not present.
+
+// 1. Write a function that takes an array of integers and returns the sum of all elements.
+// Input: [1, 2, 3, 4]
+// Output: 10
+
+console.log('----------------------------');
+// 2. Write a function that takes an array of integers and returns the average of all elements.
+//Input: [1, 2, 3, 4]
+// Output: 2.5
+
+console.log('----------------------------');
+// 3. Write a function that takes an array of strings and concatenates them together adding spaces.
+// Input: ["Hello", "World", "!"]
+// Output: "Hello World!"
+
+console.log('----------------------------');
+// 4. Write a function that takes an array of integers and returns a new array with odd numbers only.
+// Input: [1, 2, 3, 4, 7, 10];
+// Output: [1, 3, 7]
+
+console.log('----------------------------');
+// 5. Write a function that takes an array of integers and returns a new array with even numbers only.
+// Input: [1, 2, 3, 4, 7, 10]
+// Output: [1, 2, 4, 10]
+
+console.log('----------------------------');
+// 6. Write a function that takes an array and a value, and returns a new array with all occurrences of the value removed.
+// Input: ([1, 2, 3, 4, 2], 2)
+// Output: [1, 3, 4]
+
+console.log('----------------------------');
+// 7. Write a function that takes an object and returns the keys as an array.
+// Input: {name: "John", age: 30, city: "New York"}
+// Output: ["name", "age", "city"]
+
+console.log('----------------------------');
+// 8. Write a function that takes an object and returns the values as an array.
+// Input: {name: "John", age: 30, city: "New York"}
+// Output: ["John", 30, "New York"]
+
+// array is an object {0: "arrayValue1", 1: "arrayValue2", 2: "arrayValue2"};
+// ["arrayValue1", "arrayValue2", "arrayValue2"];
+// primitive values are number, string, boolean
+// other values (e.g. array, function, object) are objects
+
+console.log('----------------------------');
+// 9. Write a function that takes an object and a key, and returns true if the key exists in the object, otherwise false.
+// Input: ({name: "John", age: 30, city: "New York"}, "age")
+// Output: true
+// Input: ({name: "John", age: 30, city: "New York"}, "surname")
+// Output: false
+
+console.log('----------------------------');
+// 10. Write a function that takes an object and a key-value pair, and adds the key-value pair to the object.
+// Input: ({name: "John", age: 30}, "city", "New York")
+// Output: {name: "John", age: 30, city: "New York"}
+
+console.log('----------------------------');
+// 11. Write a function that takes an object and a key, and removes the key-value pair from the object.
+// Input: ({name: "John", age: 30, city: "New York"}, "city")
+// Output: {name: "John", age: 30}
+
+console.log('----------------------------');
+// 12. Write a function that takes two arrays of equal length and returns an object with the first array as keys and the second array as values.
+// Input: (["name", "age", "city"], ["John", 30, "New York"])
+// Output: {name: "John", age: 30, city: "New York"}
+
+// 13. Write a function that takes a nested object and a key, and returns the value of the key if it exists at any level, otherwise null.
+// Input: ({a: {b: {c: 5}}}, 'c')
+// Output: 5
+
+// 14. Write a function that takes a nested object and a key, and returns the depth of the key in the object, otherwise -1.
+// Input: ({a: {b: {c: 5}}}, 'c')
+// Output: 3
