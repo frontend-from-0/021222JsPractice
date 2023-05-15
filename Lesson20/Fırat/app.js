6743780a05d3ed44995e1fd439be1083b38d6ce8
FiratSomek <121132276+FiratSomek@users.noreply.github.com>
2023-05-15 12:17:26 +0300

    Lesson20_Fırat

--- Lesson20/Fırat/app.js
+++ Lesson20/Fırat/app.js
@@ -1,35 +1,37 @@
 // Get the input field, form and the list
-const input = document.getElementById('todo-input');
-const list = document.getElementById('todo-list');
-const form = document.getElementById('todo-form');
+const input = document.getElementById("todo-input");
+const list = document.getElementById("todo-list");
+const form = document.getElementById("todo-form");
 
 // Add a listener to the form to handle form submission
-form.addEventListener('submit', (event) => {
+form.addEventListener("submit", (event) => {
   event.preventDefault();
   // Create a new task item and add it to the list
 
-  const listItemElement=document.createElement("li");
-  listItemElement.innerText = input.value;
-  
+  const listItemElement = document.createElement("li");
+  const spanElement = document.createElement("span");
+  spanElement.innerText = input.value;
+  listItemElement.appendChild(spanElement);
+
   // Remove button
   const removeButton = document.createElement("button");
-  removeButton.innerText = 'X';
+  removeButton.innerText = "X";
   // TODO: Update styles of removeButton! (use button--light class)
+  removeButton.classList.add("button--light");
   listItemElement.appendChild(removeButton);
 
   list.appendChild(listItemElement);
 
   // Add a listener to the task item to toggle its completed status
-  listItemElement.addEventListener('click',() => {
+  spanElement.addEventListener("click", () => {
     // TODO: "completed" class is applied both to the text of the item and the button. Refactor the content of li element so that the class is only applied to the text but not to the button!
-    listItemElement.classList.toggle("completed");
+    spanElement.classList.toggle("completed");
   });
-  removeButton.addEventListener("click",()=>{
+  removeButton.addEventListener("click", () => {
+    event.stopPropagation();
     list.removeChild(listItemElement);
   });
-  
-  
+
   // Clear the input field
-input.value = "";
+  input.value = "";
 });
-  
