f37c958b656a57dd471efa8bcf619aa673386f40
ecemetin <eceyurdusevimli@gmail.com>
2023-05-28 20:02:38 +0300

    seventh example is done

--- Lesson19/Ece/script.js
+++ Lesson19/Ece/script.js
@@ -46,10 +46,14 @@ images[1].remove();
 const allImages = document.body.querySelectorAll("img");
 console.log("allImages");
 allImages.style.border = "2px solid #272932";
+
 // 6. Change the text of the first heading on the page to "Welcome to my Website".
 const firstHeading = document.getElementsByTagName("h1");
 firstHeading.textContent = "Welcome to my Website";
+
 // 7. Replace the first image on the page with a new image ("https://picsum.photos/400/200").
+const firstImage = document.getElementById("img1");
+firstImage.src = "https://picsum.photos/400/200";
 // 8. Remove the first list item from the unordered list.
 // 9. Append a new list item to the end of the ordered list with the text "Last Item".
 // 10. Add a button (in html) with "Hide images" text. When a button is clicked, change the text of the button to "Show images" and hide all the images on the web page (set display to none).
