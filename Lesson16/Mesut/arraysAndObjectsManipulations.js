07c49fa5adbada4b949f7d41f6ecaeda21c11495
Mesut OZTURK <doktormesut@gmail.com>
2023-07-21 00:34:05 +0300

    answers 2

--- Lesson16/Mesut/arraysAndObjectsManipulations.js
+++ Lesson16/Mesut/arraysAndObjectsManipulations.js
@@ -13,37 +13,81 @@ console.log('------------exercise 3----------------');
 // 3. Write a function that takes an array of strings and concatenates them together adding spaces.
 // Input: ["Hello", "World", "!"]
 // Output: "Hello World!"
-
+function arrToStr(arr){
+    let string="";
+    
+    for (let i = 0; i < arr.length; i++) {
+    string=string+" "+arr[i];
+        
+    }
+console.log(string);
+}
+arrToStr(["Hello", "World", "!"])
 console.log('-----------exercise 4-----------------');
 // 4. Write a function that takes an array of integers and returns a new array with odd numbers only.
 // Input: [1, 2, 3, 4, 7, 10];
 // Output: [1, 3, 7]
-
+function oddNumbers(arr){
+    let newArr=[]
+for(let num in arr){
+    if(arr[num]%2==!0){
+        newArr.push(arr[num]);
+    }
+   
+}
+console.log(newArr);
+}
+oddNumbers([1, 2, 3, 4, 7, 10])
 console.log('------------exercise 5----------------');
 // 5. Write a function that takes an array of integers and returns a new array with even numbers only.
 // Input: [1, 2, 3, 4, 7, 10]
 // Output: [1, 2, 4, 10]
 
+function evenNumbers(arr){
+    let newArr2=[];
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]%2===0){
+            newArr2.push(arr[i])
+        }
+    }
+    console.log(newArr2);
+}
+evenNumbers([1, 2, 3, 4, 7, 10])
 console.log('------------exercise 6----------------');
 // 6. Write a function that takes an array and a value, and returns a new array with all occurrences of the value removed.
-// Input: ([1, 2, 3, 4, 2], 2)
+// Input: ([1, 2,3,2, 4, 2,122,11,12], 2)
 // Output: [1, 3, 4]
+
+function deletedArr(arr,n){
+    let newArr4=[];
+    for (let i = 0; i < arr.length; i++) {
+      if(arr[i] !==n){
+        newArr4.push(arr[i])
+      }
+        
+    }
+    console.log(newArr4);
+}
+
+deletedArr([1, 2, 3, 4,3, 3,11,18,2,2,2,78], 3)
 
 console.log('--------------exercise 7--------------');
 // 7. Write a function that takes an object and returns the keys as an array.
 // Input: {name: "John", age: 30, city: "New York"}
 // Output: ["name", "age", "city"]
+debugger;
+function justKeys(obj){
+    let keys=[];
+    for(let i=0;i<Object.values(obj).length;i++){
+        keys.push(Object.values(obj)[i]);
+
+    }
+    console.log(keys);
+}
+
+justKeys({name: "John", age: 30, city: "New York"})
 
 console.log('------------exercise 8----------------');
-// 8. Write a function that takes an object and returns the values as an array.
-// Input: {name: "John", age: 30, city: "New York"}
-// Output: ["John", 30, "New York"]
-
-// array is an object {0: "arrayValue1", 1: "arrayValue2", 2: "arrayValue2"};
-// ["arrayValue1", "arrayValue2", "arrayValue2"];
-// primitive values are number, string, boolean
-// other values (e.g. array, function, object) are objects
-
 console.log('-------------exercise 9---------------');
 // 9. Write a function that takes an object and a key, and returns true if the key exists in the object, otherwise false.
 // Input: ({name: "John", age: 30, city: "New York"}, "age")
@@ -51,6 +95,15 @@ console.log('-------------exercise 9---------------');
 // Input: ({name: "John", age: 30, city: "New York"}, "surname")
 // Output: false
 
+function check(obj,key){
+    for(let i=0;i<Object.values(obj).length;i++){
+        if()
+    }
+
+
+
+
+}
 console.log('--------------exercise 10--------------');
 // 10. Write a function that takes an object and a key-value pair, and adds the key-value pair to the object.
 // Input: ({name: "John", age: 30}, "city", "New York")
