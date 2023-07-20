e3357a27685a83ca67d7b42430d382fac2eac6a5
Mesut OZTURK <doktormesut@gmail.com>
2023-07-20 22:01:25 +0300

    answers

--- Lesson16/Mesut/arraysAndObjectsManipulations.js
+++ Lesson16/Mesut/arraysAndObjectsManipulations.js
@@ -4,37 +4,37 @@
 // Input: [1, 2, 3, 4]
 // Output: 10
 
-console.log('----------------------------');
+console.log('-----------exercise 2-----------------');
 // 2. Write a function that takes an array of integers and returns the average of all elements.
 //Input: [1, 2, 3, 4]
 // Output: 2.5
 
-console.log('----------------------------');
+console.log('------------exercise 3----------------');
 // 3. Write a function that takes an array of strings and concatenates them together adding spaces.
 // Input: ["Hello", "World", "!"]
 // Output: "Hello World!"
 
-console.log('----------------------------');
+console.log('-----------exercise 4-----------------');
 // 4. Write a function that takes an array of integers and returns a new array with odd numbers only.
 // Input: [1, 2, 3, 4, 7, 10];
 // Output: [1, 3, 7]
 
-console.log('----------------------------');
+console.log('------------exercise 5----------------');
 // 5. Write a function that takes an array of integers and returns a new array with even numbers only.
 // Input: [1, 2, 3, 4, 7, 10]
 // Output: [1, 2, 4, 10]
 
-console.log('----------------------------');
+console.log('------------exercise 6----------------');
 // 6. Write a function that takes an array and a value, and returns a new array with all occurrences of the value removed.
 // Input: ([1, 2, 3, 4, 2], 2)
 // Output: [1, 3, 4]
 
-console.log('----------------------------');
+console.log('--------------exercise 7--------------');
 // 7. Write a function that takes an object and returns the keys as an array.
 // Input: {name: "John", age: 30, city: "New York"}
 // Output: ["name", "age", "city"]
 
-console.log('----------------------------');
+console.log('------------exercise 8----------------');
 // 8. Write a function that takes an object and returns the values as an array.
 // Input: {name: "John", age: 30, city: "New York"}
 // Output: ["John", 30, "New York"]
@@ -44,32 +44,32 @@ console.log('----------------------------');
 // primitive values are number, string, boolean
 // other values (e.g. array, function, object) are objects
 
-console.log('----------------------------');
+console.log('-------------exercise 9---------------');
 // 9. Write a function that takes an object and a key, and returns true if the key exists in the object, otherwise false.
 // Input: ({name: "John", age: 30, city: "New York"}, "age")
 // Output: true
 // Input: ({name: "John", age: 30, city: "New York"}, "surname")
 // Output: false
 
-console.log('----------------------------');
+console.log('--------------exercise 10--------------');
 // 10. Write a function that takes an object and a key-value pair, and adds the key-value pair to the object.
 // Input: ({name: "John", age: 30}, "city", "New York")
 // Output: {name: "John", age: 30, city: "New York"}
 
-console.log('----------------------------');
+console.log('-------------exercise 11---------------');
 // 11. Write a function that takes an object and a key, and removes the key-value pair from the object.
 // Input: ({name: "John", age: 30, city: "New York"}, "city")
 // Output: {name: "John", age: 30}
 
-console.log('----------------------------');
+console.log('-------------exercise 12---------------');
 // 12. Write a function that takes two arrays of equal length and returns an object with the first array as keys and the second array as values.
 // Input: (["name", "age", "city"], ["John", 30, "New York"])
 // Output: {name: "John", age: 30, city: "New York"}
-
+console.log('-------------exercise 13---------------');
 // 13. Write a function that takes a nested object and a key, and returns the value of the key if it exists at any level, otherwise null.
 // Input: ({a: {b: {c: 5}}}, 'c')
 // Output: 5
-
+console.log('-------------exercise 14---------------');
 // 14. Write a function that takes a nested object and a key, and returns the depth of the key in the object, otherwise -1.
 // Input: ({a: {b: {c: 5}}}, 'c')
 // Output: 3
