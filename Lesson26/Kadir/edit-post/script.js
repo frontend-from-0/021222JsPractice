32e126b8d749e24ced81355ab6f68169ed809076
Kadcihsof <87387215+Kadcihsof@users.noreply.github.com>
2023-07-23 16:09:01 +0200

    34

--- Lesson26/Kadir/edit-post/script.js
+++ Lesson26/Kadir/edit-post/script.js
@@ -82,12 +82,11 @@ fetch('https://jsonplaceholder.typicode.com/posts/'+'?postId=postId', {
   .then((json) => console.log(json));
 
   form.addEventListener("submit", (event) => editPost(event));
-  
+} 
 /* 
 TODO: add logic and necessary element to the HTML file to create a form where a post can be edited.
 When page is loaded, the default values should contain current title and body of the post.
 It should be possible to send an API request (PUT) to update the post.
 The result of the API call should be displayed on the same page under the form, if the API request was successful.
 The page should containt a button to return back to the main page.
-
 */
