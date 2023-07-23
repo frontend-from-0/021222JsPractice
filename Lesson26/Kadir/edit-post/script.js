47d8875ce7bd8dae7a9e868e7b97cdb7aef04107
Kadcihsof <87387215+Kadcihsof@users.noreply.github.com>
2023-07-23 15:50:12 +0200

    32

--- Lesson26/Kadir/edit-post/script.js
+++ Lesson26/Kadir/edit-post/script.js
@@ -28,10 +28,42 @@ window.addEventListener('load', () => {
     //     });
 //   });
 
+// onst URL = 'https://jsonplaceholder.typicode.com/posts';
+// const postsContainer = document.getElementById('posts-container');
+
+// function fetchPosts() {
+// 	fetch(URL)
+// 		.then((response) => response.json())
+// 		.then((posts) =>
+// 			posts.forEach((post) => {
+// 				const listItem = document.createElement('li');
+// 				listItem.classList.add('post');
+
+// 				const heading = document.createElement('h2');
+// 				heading.classList.add('post-title');
+// 				heading.innerText = post.title;
+// 				listItem.appendChild(heading);
+
+// 				const paragraph = document.createElement('p');
+// 				paragraph.classList.add('post-body');
+// 				paragraph.innerText = post.body;
+// 				listItem.appendChild(paragraph);
+
+// 				postsContainer.appendChild(listItem);
+// 			})
+// 		)
+//     .catch((error) => {
+// 			console.error('An error occured: ', error);
+// 		});
+// };
+
+function editPost(event) {
+	event.preventDefault();
+
 const titleEdit = document.getElementById('edit-title');  
 const bodyEdit = document.getElementById('edit-body');
 
-fetch('https://jsonplaceholder.typicode.com/posts/'+'?postId=${post.id}', {
+fetch('https://jsonplaceholder.typicode.com/posts/'+'?postId=postId', {
   method: 'PUT',
   body: JSON.stringify({
     id: postId,
@@ -45,7 +77,8 @@ fetch('https://jsonplaceholder.typicode.com/posts/'+'?postId=${post.id}', {
 })
   .then((response) => response.json())
   .then((json) => console.log(json));
-  
+
+  form.addEventListener("submit", (event) => editPost(event));
   
 /* 
 TODO: add logic and necessary element to the HTML file to create a form where a post can be edited.
