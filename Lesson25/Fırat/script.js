b6064551457308b7d940beaf5ad63b93074035e1
FiratSomek <121132276+FiratSomek@users.noreply.github.com>
2023-08-02 17:16:03 +0300

    Update script.js

--- Lesson25/Fırat/script.js
+++ Lesson25/Fırat/script.js
@@ -5,12 +5,35 @@ h2 (.post-title)
 p (.post-body)
 */
 
-const URL = 'https://jsonplaceholder.typicode.com/posts';
+const URL = "https://jsonplaceholder.typicode.com/posts";
+const postList = document.getElementById("post-list");
 function fetchPosts() {
+  fetch(URL)
+    .then((response) => response.json())
+    .then((posts) => {
+      posts.foreach((post) => {
+        const postList = document.createElement("li");
+        postList.classList.add("post");
+
+        const postTitle = document.createElement("h2");
+        postTitle.classList.add("post-title");
+        postTitle.innerText = post.title;
+        postList.appendChild(postTitle);
+
+        const postBody = document.createElement("p");
+        postBody.classList.add('post-body');
+        postBody.innerText = post.body;
+        postList.appendChild('postBody');
+      });
+    });
+    .catch((error)=>{
+        console.error(error);
+    })
 }
 
 function clearPosts() {
-};
+  postList.innerHTML = " ";
+}
 
-document.getElementById('fetch-button').addEventListener('click', fetchPosts);
-document.getElementById('clear-button').addEventListener('click', clearPosts);
+document.getElementById("fetch-button").addEventListener("click", fetchPosts);
+document.getElementById("clear-button").addEventListener("click", clearPosts);
