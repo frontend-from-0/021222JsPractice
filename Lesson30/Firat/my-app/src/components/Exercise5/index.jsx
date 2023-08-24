51164582561e74a8bf672ad801e80d1a2d765c77
FiratSomek <121132276+FiratSomek@users.noreply.github.com>
2023-08-24 18:33:55 +0300

    Create index.jsx

---
+++ Lesson30/Firat/my-app/src/components/Exercise5/index.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+
+export const Exercise5 = () => {
+  const [coordinates, setCoordinates] = useState({ x: 0, y: 0 });
+  const handleClick = (event) => {
+    const { clientX, clientY } = event;
+    setCoordinates({ x: clientX, y: clientY });
+  };
+  return (
+    <div>
+      <button onClick={handleClick}>View Mouse Coordinates</button>
+      <p>x coordinate: {coordinates.x}</p>
+      <p>y coordinate: {coordinates.y}</p>
+    </div>
+  );
+};
