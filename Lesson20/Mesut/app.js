d015180e2f25dd9fe21f2fbe594aace2bd79245f
Mesut Öztürk <drmst@users.noreply.github.com>
2023-08-24 20:07:53 +0300

    Update app.js

--- Lesson20/Mesut/app.js
+++ Lesson20/Mesut/app.js
@@ -1,35 +1,32 @@
-// Get the input field, form and the list
-const input = document.getElementById('todo-input');
-const list = document.getElementById('todo-list');
-const form = document.getElementById('todo-form');
+const input = document.getElementById("todo-input");
+const list = document.getElementById("todo-list");
+const form = document.getElementById("todo-form");
 
-// Add a listener to the form to handle form submission
-form.addEventListener('submit', (event) => {
+form.addEventListener("submit", (event) => {
   event.preventDefault();
-  // Create a new task item and add it to the list
 
-  const listItemElement=document.createElement("li");
-  listItemElement.innerText = input.value;
-  
-  // Remove button
-  const removeButton = document.createElement("button");
-  removeButton.innerText = 'X';
-  // TODO: Update styles of removeButton! (use button--light class)
-  listItemElement.appendChild(removeButton);
+  if (input.value !== "") {
+    const divElement = document.createElement("div");
+    divElement.classList.add("container-div");
+    const listItemElement = document.createElement("p");
+    listItemElement.className = "list-item";
+    listItemElement.innerText = input.value;
+    const removeButton = document.createElement("button");
+    removeButton.innerText = "X";
+    removeButton.className = "remove-button";
 
-  list.appendChild(listItemElement);
+    list.appendChild(divElement);
+    divElement.appendChild(listItemElement);
+    divElement.appendChild(removeButton);
 
-  // Add a listener to the task item to toggle its completed status
-  listItemElement.addEventListener('click',() => {
-    // TODO: "completed" class is applied both to the text of the item and the button. Refactor the content of li element so that the class is only applied to the text but not to the button!
-    listItemElement.classList.toggle("completed");
-  });
-  removeButton.addEventListener("click",()=>{
-    list.removeChild(listItemElement);
-  });
-  
-  
-  // Clear the input field
-input.value = "";
+    listItemElement.addEventListener("click", () => {
+      listItemElement.classList.toggle("completed");
+    });
+
+    removeButton.addEventListener("click", () => {
+      list.removeChild(divElement);
+    });
+  }
+
+  input.value = "";
 });
-  
