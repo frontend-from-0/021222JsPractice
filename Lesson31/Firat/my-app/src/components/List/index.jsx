f8a09dd668e413fc9679efcda0a759027796dac3
FiratSomek <121132276+FiratSomek@users.noreply.github.com>
2023-08-30 19:15:58 +0300

    Update index.jsx

--- Lesson31/Firat/my-app/src/components/List/index.jsx
+++ Lesson31/Firat/my-app/src/components/List/index.jsx
@@ -1,34 +1,52 @@
-import {ListItem} from '../ListItem';
-import {useState} from "react";
-import './styles.css';
+import { ListItem } from "../ListItem";
+import { NewItem } from "../NewItem";
+import { useState } from "react";
+import "./styles.css";
 
 export const List = () => {
-  const [data, setData]  = useState([
-    {id: 1, title: "Do dishes", completed: false},
-    {id: 2, title: "Do homework", completed: true},
-    {id: 3, title: "Go running", completed: false},
-    {id: 4, title: "Do dishes 2", completed: false},
-    {id: 5, title: "Do homework 3", completed: false},
-    {id: 6, title: "Go running 4", completed: false},
+  const [data, setData] = useState([
+    { id: 1, title: "Do dishes", completed: false },
+    { id: 2, title: "Do homework", completed: true },
+    { id: 3, title: "Go running", completed: false },
+    { id: 4, title: "Do dishes 2", completed: false },
+    { id: 5, title: "Do homework 3", completed: false },
+    { id: 6, title: "Go running 4", completed: false },
   ]);
 
   const handleClick = (id) => {
     setData((prevData) => {
-      return prevData.map(dataItem => 
-        dataItem.id === id ? {...dataItem, completed: !dataItem.completed} : dataItem)
+      return prevData.map((dataItem) =>
+        dataItem.id === id
+          ? { ...dataItem, completed: !dataItem.completed }
+          : dataItem
+      );
     });
   };
-  
-return (
-  <ul className="todo-list">
-      {
-        data.map(listItem => 
-        <ListItem key={listItem.id}
-        id={listItem.id}
-        title={listItem.title}
-        completed={listItem.completed}
-        handleClick={handleClick}/>)
-      }
-  </ul>
-)
+  const handleDelete = (id) => {
+    setData((prevData) => prevData.filter((dataItem) => dataItem.id !== id));
+  };
+
+  const handleAddItem = (text) => {
+    const maxId = Math.max(...data.map((item) => item.id));
+    const newItem = { id: maxId + 1, title: text, completed: false };
+    setData([...data, newItem]);
+  };
+
+  return (
+    <div>
+      <NewItem handleAddItem={handleAddItem} />
+      <ul className="todo-list">
+        {data.map((listItem) => (
+          <ListItem
+            key={listItem.id}
+            id={listItem.id}
+            title={listItem.title}
+            completed={listItem.completed}
+            handleClick={handleClick}
+            handleDelete={handleDelete}
+          />
+        ))}
+      </ul>
+    </div>
+  );
 };
