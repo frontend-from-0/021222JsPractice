7cac787b1eead50a268a5fd81568414cd06bc544
FiratSomek <121132276+FiratSomek@users.noreply.github.com>
2023-08-30 19:16:09 +0300

    Create index.jsx

---
+++ Lesson31/Firat/my-app/src/components/NewItem/index.jsx
@@ -0,0 +1,27 @@
+import "./styles.css";
+import React, { useState } from "react";
+
+export const NewItem = ({ handleAddItem }) => {
+  const [text, setText] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (text.trim() !== "") {
+      handleAddItem(text);
+      setText("");
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        className="newItemInput"
+        type="text"
+        placeholder="Enter a new task"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
