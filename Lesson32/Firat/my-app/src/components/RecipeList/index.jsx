b90091c64e3d8cb5826fa26d487fd5f5e506b261
FiratSomek <121132276+FiratSomek@users.noreply.github.com>
2023-09-09 15:44:36 +0300

    Update index.jsx

--- Lesson32/Firat/my-app/src/components/RecipeList/index.jsx
+++ Lesson32/Firat/my-app/src/components/RecipeList/index.jsx
@@ -1,13 +1,25 @@
-export const RecipeList = ({recipes}) => {
+import "./styles.css";
 
-    return(
-        <div>
-            <h2>Search Results</h2>
-            <ul>
-              {
-             recipes.map(recipe => <li key={recipe.idMeal} >{recipe.strMeal}</li>)   
-              }
-            </ul>
-        </div>
-    )
-}
+export const RecipeList = ({ recipes, handleRecipeSelect, selectedRecipe }) => {
+  return (
+    <div className="recipe-container">
+      <h2>Search Results</h2>
+      <ul className="recipe-list">
+        {recipes.map((recipe) => (
+          <li
+            key={recipe.idMeal}
+            onClick={() => handleRecipeSelect(recipe)}
+            style={{
+              backgroundColor:
+                selectedRecipe && selectedRecipe.idMeal === recipe.idMeal
+                  ? "lightblue" // Background color of the clicked item
+                  : "", // Background color of other items, its empty because ı did it color in other component
+            }}
+          >
+            {recipe.strMeal}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
