aa23d7b06357ab0bf30a6fd8842f607b3cc09d1d
FiratSomek <121132276+FiratSomek@users.noreply.github.com>
2023-09-09 15:44:41 +0300

    Update index.jsx

--- Lesson32/Firat/my-app/src/components/SearchBar/index.jsx
+++ Lesson32/Firat/my-app/src/components/SearchBar/index.jsx
@@ -1,28 +1,33 @@
-import {useState} from "react";
-import { BD_SEARCH_BASE_URL } from '../../urls';
+import { useState } from "react";
+import { BD_SEARCH_BASE_URL } from "../../urls";
+import "./styles.css";
 
-export const SearchBar=({setRecipes})=>{
-  const [searchQuery, setSearchQuery]= useState('');
+export const SearchBar = ({ setRecipes }) => {
+  const [searchQuery, setSearchQuery] = useState("");
 
-    const handleSearch=()=>{
-      console.log(BD_SEARCH_BASE_URL + searchQuery);
-        fetch(BD_SEARCH_BASE_URL + searchQuery)
-        .then(response=>response.json())
-        .then(data => {
-          if (data.meals) return setRecipes(data.meals);
-        })
-        .catch(error => setRecipes([]));
-    };
-    
-    return(
-        <form action="#">
-          <input
-            type="text"
-            placeholder="Search for recepie"
-            onChange={e => setSearchQuery(e.target.value)}
-          />
-          <input type="submit" value="Search" onClick={handleSearch}/>
-        </form>
-        
-    )
-}
+  const handleSearch = () => {
+    console.log(BD_SEARCH_BASE_URL + searchQuery);
+    fetch(BD_SEARCH_BASE_URL + searchQuery)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.meals) return setRecipes(data.meals);
+      })
+      .catch((error) => setRecipes([]));
+  };
+
+  return (
+    <form action="#">
+      <input
+        type="text"
+        placeholder="Search for recipe"
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
+      <input
+        className="input-btn"
+        type="submit"
+        value="Search"
+        onClick={handleSearch}
+      />
+    </form>
+  );
+};
