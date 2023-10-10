ac35e71fc28b20c5b3916f90cf4210cc568c0cbc
FiratSomek <121132276+FiratSomek@users.noreply.github.com>
2023-10-10 19:53:23 +0300

    Update index.jsx

--- Lesson34/firat/my-app/src/components/RecipeList/index.jsx
+++ Lesson34/firat/my-app/src/components/RecipeList/index.jsx
@@ -1,12 +1,33 @@
-export const RecipeList = ({ recipes, onRecipeClick }) => {
-	return (
-		<div>
-			<h2>Search Results</h2>
-			<ul>
-				{recipes.map((recipe) => (
-					<li key={recipe.idMeal} onClick={() => onRecipeClick(recipe)}>{recipe.strMeal}</li>
-				))}
-			</ul>
-		</div>
-	);
+import "./styles.css";
+
+export const RecipeList = ({ recipes, handleSelectRecipe, selectedRecipe }) => {
+  return (
+    <div className="recipes-board">
+      <h1 className="recipes-title">Just For You</h1>{" "}
+      <ul className="recipe-list">
+        {recipes.map((recipe) => (
+          <li
+            className="recipe-item"
+            key={recipe.idMeal}
+            onClick={() => handleSelectRecipe(recipe)}
+            style={{
+              display: selectedRecipe
+                ? selectedRecipe.idMeal === recipe.idMeal
+                  ? "block"
+                  : "none"
+                : "block",
+            }}
+          >
+            <img
+              className="recipe-img"
+              src={recipe.strMealThumb}
+              alt={recipe.strMeal}
+            />
+            <p style={{ margin: "5px", fontWeight: 400 }}>{recipe.strMeal}</p>
+            <p style={{ margin: "5px", color: "grey" }}>{recipe.strArea}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
