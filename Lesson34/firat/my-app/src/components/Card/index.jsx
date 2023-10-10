59db2752adb874f5a3915575bfd09377b0f6e6a9
FiratSomek <121132276+FiratSomek@users.noreply.github.com>
2023-10-10 19:53:02 +0300

    Delete index.jsx

--- Lesson34/firat/my-app/src/components/Card/index.jsx
+++
@@ -1,7 +0,0 @@
-import './styles.css';
-
-export const Card = ({ dark, children }) => {
-  const classes = dark ? 'card card--dark' : 'card';
-
-  return <div className={classes}>{children}</div>;
-};
