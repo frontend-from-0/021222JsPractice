a54ed9fbf767c7581663116488b7779454647c44
FiratSomek <121132276+FiratSomek@users.noreply.github.com>
2023-10-10 19:53:00 +0300

    Update App.jsx

--- Lesson34/firat/my-app/src/App.jsx
+++ Lesson34/firat/my-app/src/App.jsx
@@ -1,40 +1,88 @@
-import { SearchBar } from './components/SearchBar';
-import { useEffect, useState } from 'react';
-import { RecipeList } from './components/RecipeList';
-import {RecipeDetail} from './components/RecipeDetail';
-import { BD_SEARCH_BASE_URL } from './urls';
-import { Card } from './components/Card';
-import {ReactHookFormRegistrationForm} from './components/ReactHookFormRegistrationForm';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { BD_SEARCH_BASE_URL } from "./urls";
+import { RecipeList } from "./components/RecipeList";
+import { Categories } from "./components/Categories";
+import { SearchButton } from "./components/SearchButton";
+import { Header } from "./components/Header";
+import { RecipeDetail } from "./components/RecipeDetail";
 
-import './App.css';
+function App() {
+  const [recipes, setRecipes] = useState([]);
+  const [filteredRecipes, setFilteredRecipes] = useState([]);
+  const [selectedRecipe, setSelectedRecipe] = useState();
+  const [searchQuery, setSearchQuery] = useState("");
 
-export const App = () => {
-	const [recipes, setRecipes] = useState([]);
-  const [selectedRecipe, setSelectedRecipe] = useState(undefined);
+  const handleSearch = () => {
+    console.log(BD_SEARCH_BASE_URL + searchQuery);
+    fetch(BD_SEARCH_BASE_URL + searchQuery)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.meals) return setRecipes(data.meals);
+      })
+      .catch((error) => setRecipes([]));
+  };
 
-	useEffect(() => {
-		fetch(BD_SEARCH_BASE_URL)
-			.then((response) => response.json())
-			.then((data) => {
-				if (data.meals) return setRecipes(data.meals);
-			})
-			.catch((error) => setRecipes([]));
-	}, []);
+  const fetchData = () => {
+    fetch(BD_SEARCH_BASE_URL)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.meals) return setRecipes(data.meals);
+      })
+      .catch((error) => setRecipes([]));
+  };
 
-	return (
-		<div className='app-container'>
-			<header>
-				{selectedRecipe ? <button className='header__back-button' onClick={() => setSelectedRecipe(null)}>Go back</button>:<></>}
-				<h1>Recipe Search App</h1>
-			</header>
-			<Card>
-				<ReactHookFormRegistrationForm />
-			</Card>
-			<Card>
-				{!selectedRecipe ? <SearchBar setRecipes={setRecipes} />: <></>}
-				{!selectedRecipe ? <RecipeList recipes={recipes} onRecipeClick={setSelectedRecipe}/>: <></>}
-      	{selectedRecipe ? <RecipeDetail recipe={selectedRecipe} />:<></>}
-			</Card>
-		</div>
-	);
-};
+  useEffect(fetchData, []);
+
+  const filterRecipesByCategory = (category) => {
+    const filtered = recipes.filter(
+      (recipe) => recipe.strCategory === category
+    );
+    setFilteredRecipes(filtered);
+  };
+
+  const handleSelectRecipe = (recipe) => {
+    setSelectedRecipe(recipe);
+  };
+
+  const handleCloseRecipeDetail = () => {
+    setSelectedRecipe(null);
+  };
+
+  return (
+    <div className="App">
+      <Header
+        filterRecipesByCategory={filterRecipesByCategory}
+        setSearchQuery={setSearchQuery}
+        fetchData={fetchData}
+        setSelectedRecipe={setSelectedRecipe}
+      />
+      <SearchButton
+        handleSearch={handleSearch}
+        setSearchQuery={setSearchQuery}
+        filterRecipesByCategory={filterRecipesByCategory}
+        recipes={recipes}
+        searchQuery={searchQuery}
+      />
+      <Categories
+        recipes={recipes}
+        filterRecipesByCategory={filterRecipesByCategory}
+      />
+      <RecipeList
+        recipes={filteredRecipes.length > 0 ? filteredRecipes : recipes}
+        handleSelectRecipe={handleSelectRecipe}
+        selectedRecipe={selectedRecipe}
+      />
+      {selectedRecipe ? (
+        <RecipeDetail
+          selectedRecipe={selectedRecipe}
+          onClose={handleCloseRecipeDetail}
+        />
+      ) : (
+        ""
+      )}
+    </div>
+  );
+}
+
+export default App;
