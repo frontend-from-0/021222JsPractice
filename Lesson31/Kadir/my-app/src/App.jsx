dcb790e9102486851b1a91112c465c27f5afcf85
Kadcihsof <87387215+Kadcihsof@users.noreply.github.com>
2023-12-06 22:34:03 +0100

    1

--- Lesson31/Kadir/my-app/src/App.jsx
+++ Lesson31/Kadir/my-app/src/App.jsx
@@ -26,3 +26,7 @@ export const App = () => {
     </AppContext.Provider>
   );
 };
+
+
+// Try to finish the Todo list application that we did in the lesson.
+// Add input to add new todo items, a submit button and a delete button to each of the list elements.
