14d1473b5fe073b2db38c520e0cfbfce7bc54630
Kadcihsof <87387215+Kadcihsof@users.noreply.github.com>
2024-01-07 15:32:00 +0100

    final

--- Lesson19/Kadir/script.js
+++ Lesson19/Kadir/script.js
@@ -78,7 +78,7 @@ liItems[0].remove();
 
 // 10. Add a button (in html) with "Hide images" text. When a button is clicked, change the text of the button to "Show images" and hide all the images on the web page (set display to none).
     const newButton = document.createElement('button');
-    newButton.innerHTML = "Hide images";
+    newButton.innerText = "Hide images";
     document.getElementById('main').appendChild(newButton);
     let shouldShowImages=true;
     newButton.addEventListener("click", toggleImages);
@@ -88,7 +88,7 @@ liItems[0].remove();
         // if (false) {
         //   images[0].style.display="block";
         // } else {
-        if(shouldShowImages==true){
+        if(shouldShowImages){
           newButton.innerHTML = "Show images";
           images[0].style.display="none";
           shouldShowImages==false;
