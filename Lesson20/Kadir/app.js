8f19d98163a9844c0293c27b5872a96707de72f9
Kadcihsof <87387215+Kadcihsof@users.noreply.github.com>
2024-01-09 20:57:50 +0100

    final

--- Lesson20/Kadir/app.js
+++ Lesson20/Kadir/app.js
@@ -23,27 +23,10 @@ form.addEventListener('submit', (event) => {
   // Add a listener to the task item to toggle its completed status
   spanElement.addEventListener('click',() => {
     // TODO: "completed" class is applied both to the text of the item and the button. Refactor the content of li element so that the class is only applied to the text but not to the button!
-
-   
-    // listItemElement.classList.toggle("completed");
-    // var a = document.listItemElement.value
-  //  input.value.classList.toggle("completed");
-    // listItemElement.parentNode.classList.remove(removeButton);
-    // removeButton.classList.deactivateClass(completed);
-
     spanElement.classList.toggle("completed");
-
   });
   
-  // removeButton.removeAttribute('text-decoration');
-  // removeButton.classList.deactivateClass(completed);
-  // removeButton.classList.remove("completed");
-
-  // removeButton.style.removeProperty("textDecoration");
-  // removeButton.style.textDecoration='none';
   removeButton.classList.add("button--light");
-  
- 
 
   removeButton.addEventListener("mouseover",()=>{
     removeButton.classList.add("button--light:hover");
