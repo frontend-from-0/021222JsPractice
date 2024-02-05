91cc7365a913ff43980bcb225a5de30b56fea61a
FiratSomek <121132276+FiratSomek@users.noreply.github.com>
2024-02-05 22:29:07 +0300

    Create index.jsx

---
+++ Lesson30/Firat/my-app/src/components/Exercise7/index.jsx
@@ -0,0 +1,24 @@
+export const Exercise7 = () => {
+  const handleParentClick = () => {
+    console.log("Parent clicked!");
+  };
+
+  const handleChildClick = () => {
+    console.log("Child clicked!");
+  };
+
+  return (
+    <div
+      onClick={handleParentClick}
+      style={{ border: "2px solid blue", padding: "10px" }}
+    >
+      <h2>Parent</h2>
+      <div
+        onClick={handleChildClick}
+        style={{ border: "2px solid red", padding: "10px" }}
+      >
+        <h3>Child</h3>
+      </div>
+    </div>
+  );
+};
